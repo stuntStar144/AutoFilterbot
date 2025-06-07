0ed802644506f8862741427a3e6e67dc69a7f3bc
Stunt <starstunt14@gmail.com>
2025-06-07 17:26:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # PyroClient Setup 
 API_ID = int(environ.get('API_ID','27183261'))
 API_HASH = environ.get('API_HASH','f32586203fd2d814ea949ad984839c61')
-BOT_TOKEN = environ.get('BOT_TOKEN','7754923786:AAEJiTGq4_naLDv4rrmxh_cT6FkhOtKT-TU')
+BOT_TOKEN = environ.get('BOT_TOKEN','')
 
 # Bot settings
 WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
@@ -33,7 +33,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://starstunt14:kce9es74owxmchsM@cluster0.myuv8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DATABASE_NAME = environ.get('DATABASE_NAME', "starstunt14")
+DATABASE_NAME = environ.get('DATABASE_NAME', "")
 FILE_DB_URL = environ.get("FILE_DB_URL", DATABASE_URL)
 FILE_DB_NAME = environ.get("FILE_DB_NAME", DATABASE_NAME)
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
